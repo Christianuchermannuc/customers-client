67e0e6da390bcf659089531f9db4873c3031581d
Christian Uchermann <christian_uchermann@hotmail.com>
2020-11-20 20:55:51 +0100

    Added header and customer main container

---
+++ customer-client/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './header.module.css';
+
+const headerItem = (props) => (
+    <header className={styles.Toolbar}>
+        <h1>
+            Christian
+        </h1>
+    </header>
+);
+
+export default headerItem;
