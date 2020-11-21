c39bfa03d2008a615a82bf3ce87d49a534cdcd74
Christian Uchermann <christian_uchermann@hotmail.com>
2020-11-21 14:05:11 +0100

    added table

--- customer-client/src/containers/customers.js
+++ customer-client/src/containers/customers.js
@@ -1,27 +1,49 @@
-import React, { useState, useEffect,useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios'
-//import styles from './header.module.css';
+import styles from './customer.module.css';
 
 const Customers = (props) => {
 
-    const [customers, setCustomers] = useState({ customers: [] })
+    const [customersList, setCustomersList] = useState([])
 
-    const fetchMyAPI = useCallback(async () => {
+    const getCustomers = useCallback(async () => {
         const result = await axios(
-             'https://customersapi-cu.azurewebsites.net/api/customers',           
-         );
-         console.log('her er customers: ', result.data)
-         setCustomers(result.data);
-      }, [])
+            'https://customersapi-cu.azurewebsites.net/api/customers',
+        );
+        console.log('her er customers: ', result.data)
+        setCustomersList(result.data);
+      console.log('customers: ',customersList )  
+    }, [])
 
     useEffect(() => {
-        fetchMyAPI()
-    }, [fetchMyAPI]);
+        getCustomers()
+    }, [getCustomers]);
+
+    const renderTableData = () => {
+        return customersList.map((customer, index) => {
+            const { id, year, numberOfOwners, type } = customer //destructuring
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{year}</td>
+                    <td>{numberOfOwners}</td>
+                    <td>{type}</td>
+                </tr>
+            )
+        })
+    }
 
     return (
-        <h1>
-            Main Contenet
-        </h1>
+        <>
+            <h1>
+                Main Contenet
+            </h1>
+            <table id='customersId' className={styles}>
+               <tbody>
+                 {renderTableData()}
+               </tbody>
+            </table>
+        </>
     )
 
 }
