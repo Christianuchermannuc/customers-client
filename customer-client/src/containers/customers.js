5dc59f71e03ff7757c44b33c14e08544a3168cc1
Christian Uchermann <christian_uchermann@hotmail.com>
2020-11-21 13:30:20 +0100

    calling api

--- customer-client/src/containers/customers.js
+++ customer-client/src/containers/customers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useCallback } from 'react';
 import axios from 'axios'
 //import styles from './header.module.css';
 
@@ -6,14 +6,17 @@ const Customers = (props) => {
 
     const [customers, setCustomers] = useState({ customers: [] })
 
-    useEffect(async () => {
+    const fetchMyAPI = useCallback(async () => {
         const result = await axios(
-           // 'https://customersapi-cu.azurewebsites.net/api/customers',
-           'http://localhost:5000/api/customers',
-        );
-        console.log('her er customers: ', result.data)
-        setCustomers(result.data);
-    }, []);
+             'https://customersapi-cu.azurewebsites.net/api/customers',           
+         );
+         console.log('her er customers: ', result.data)
+         setCustomers(result.data);
+      }, [])
+
+    useEffect(() => {
+        fetchMyAPI()
+    }, [fetchMyAPI]);
 
     return (
         <h1>
